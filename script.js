100462a3d7e7defbc4f760950c5c473877dde2b1
rafaelb056 <rafaelbenitez@gmail.com>
2024-06-12 13:25:46 -0400

    add script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function getComputerChoice () {
-	let computer = ["rock", "paper", "scissors"];
+	let computer = ["rock", "paper", "scissor"];
 	let random = computer[Math.floor(Math.random() * computer.length)];
 	return random;
 }
 
 function getHumanChoice(){
 	let selection = prompt("Choose Rock, Paper, or Scissor:")
-	let human = selection;
+	let human = selection.toLowerCase();
 	return human
 }
 
@@ -17,16 +17,22 @@ function playRound(humanChoice, computerChoice){
 	if(humanChoice == computerChoice){
 		return "tie";
 	}else if(humanChoice == "rock" && computerChoice == "paper"){
+		computerScore ++;
 		return "You loses! paper beats rock";
-	}else if(humanChoice =="scissor"&& computerChoice== "paper"){
+	}else if(humanChoice =="scissor" && computerChoice == "paper"){
+		humanScore++;
 		return "You win! scissor beats paper";
-	}else if(humanChoice =="rock"&& computerChoice=="scissor"){
+	}else if(humanChoice =="rock" && computerChoice =="scissor"){
+		humanScore++;
 		return "You win! rock beats scissor";
-	}else if(humanChoice=="paper" && computerChoice=="rock"){
+	}else if(humanChoice =="paper" && computerChoice =="rock"){
+		humanScore ++;
 		return "You win! paper beats rock";
-	}else if(humanChoice=="paper"&& computerChoice=="scissor"){
+	}else if(humanChoice =="paper" && computerChoice =="scissor"){
+		computerScore ++;
 		return "You loses! scissor beats paper";
-	}else if(humanChoice=="scissor" && computerChoice=="rock"){
+	}else if(humanChoice =="scissor" && computerChoice =="rock"){
+		computerScore ++;
 		return "You loses! rock beats scissor";
 	}
 }
@@ -34,4 +40,7 @@ function playRound(humanChoice, computerChoice){
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
+function playGame() {
+	playRound(humanChoice, computerChoice);
+}
 console.log(playRound(humanSelection, computerSelection));
